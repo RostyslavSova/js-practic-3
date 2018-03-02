83b785875cb8a877c43befbfa94ad5b465fb238d
Rostyslav Sova <sova.rostislav@gmail.com>
2018-03-02 17:25:43 +0000

    initial

---
+++ js/jqery-tabs.js
@@ -0,0 +1,11 @@
+$('.buttons button').click(e =>{
+    const tabNum = e.target.dataset.tab;
+    $('.buttons button, .tabs div')
+        .each(i, el => {
+            if (el.dataset.tab === tabNum) {
+                el.classList.add('active');
+            } else {
+                el.classList.remove('active');
+            }
+        } );
+});
